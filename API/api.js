01c689b777227774c528b9c700fc8e766b025820
orimolco102 <orimolco102@gmail.com>
2026-09-02 20:55:59 +0300

    env is dumb

--- API/api.js
+++ API/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const app = express();
 
 
